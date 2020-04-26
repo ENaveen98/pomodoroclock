b7fcb200aacc27d5ba3cb117c10104330ee527a6
ENaveen98 <enaveen98@gmail.com>
2020-04-27 00:38:14 +0530

    Updates

---
+++ precache-manifest.df5c654d81138ed62857f995763068a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1b9f25b7de18e3dc1b781c7cc6bc46",
+    "url": "/pomodoroclock/index.html"
+  },
+  {
+    "revision": "18ff85fc839df52447e0",
+    "url": "/pomodoroclock/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "688dc9b7d975bad9883b",
+    "url": "/pomodoroclock/static/js/2.9b42f836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoroclock/static/js/2.9b42f836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ff85fc839df52447e0",
+    "url": "/pomodoroclock/static/js/main.ca5989b6.chunk.js"
+  },
+  {
+    "revision": "9c0a38780dfec464922e",
+    "url": "/pomodoroclock/static/js/runtime-main.34e6b38f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoroclock/static/media/logo.5d5d9eef.svg"
+  }
+]);
